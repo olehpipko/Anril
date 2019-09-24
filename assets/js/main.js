73e2b75c73538b25ff5b8f71a3eb69311165988c
Oleshka24 <i.oleshka24@gmail.com>
2019-09-24 17:52:37 +0300

    Desktop

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,13 @@
 let navItems = document.querySelectorAll('.header__nav-item'),
+
+	headerBar = document.querySelector('.header__bar'),
+
 	servicesBtns = document.querySelectorAll('.services__nav-item'),
-	servicesItems = document.querySelectorAll('.services__item');
+	servicesItems = document.querySelectorAll('.services__item'),
+
+	bannerBtn = document.querySelector('.banner__btn'),
+	bannerForm = document.querySelector('.banner__form'),
+	bannerContent = document.querySelector('.banner__content');
 
 for (let i = servicesBtns.length - 1; i >= 0; i--) {
 	servicesBtns[i].onclick = function () {
@@ -12,8 +19,19 @@ for (let i = servicesBtns.length - 1; i >= 0; i--) {
 
 		servicesBtns[i].classList.add('services__nav-item--current');
 		servicesItems[i].classList.add('services__item--current');
-		
-		console.log(this);
 
 	}
 }
+
+bannerBtn.onclick = function() {
+	bannerForm.classList.remove('banner__form--hide');
+	bannerContent.classList.remove('banner__content--current');
+}
+
+window.addEventListener('scroll', function() {
+	if( pageYOffset >= 66) {
+		headerBar.classList.add('header__bar--fixed');
+	} else {
+		headerBar.classList.remove('header__bar--fixed');
+	}
+});
