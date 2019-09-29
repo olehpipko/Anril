758e928015f4b28c57d6b8a37fb1871d71839094
Oleshka24 <i.oleshka24@gmail.com>
2019-09-29 12:49:48 +0300

    Adaptive

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,13 @@ let navItems = document.querySelectorAll('.header__nav-item'),
 	bannerForm = document.querySelector('.banner__form'),
 	bannerContent = document.querySelector('.banner__content'),
 
-	conditionsItem = document.querySelectorAll('.conditions__item');
+	conditionsItem = document.querySelectorAll('.conditions__item'),
+	portfolioItem = document.querySelectorAll('.portfolio__item'),
+
+	headerNav = document.querySelector('.header__nav'),
+	headerNavBtn = document.querySelector('.header__nav-btn'),
+
+	headerNavHaveMegaMenu = document.querySelectorAll('.header__nav-item--have-megamenu');
 
 for (let i = servicesBtns.length - 1; i >= 0; i--) {
 	servicesBtns[i].onclick = function () {
@@ -51,5 +57,40 @@ if ( document.documentElement.clientWidth < 992 ) {
 		conditionsItem[i].onclick = function () {
 			this.classList.add('conditions__item--current');
 		}
+	}
+
+	for(let i = 0; i < portfolioItem.length; i++) {
+		portfolioItem[i].onclick = function () {
+			this.classList.add('portfolio__item--current');
+		}
+	}
+
+	for (let i = headerNavHaveMegaMenu.length - 1; i >= 0; i--) {
+		arrowButton = document.createElement('button');
+		arrowButton.classList.add('header__subnav-open');
+		headerNavHaveMegaMenu[i].append(arrowButton);
+		arrowButton.onclick = function() {
+
+			megaMenu = this.previousElementSibling;
+			btn = this;
+
+			if ( megaMenu.classList.contains('header__megamenu--active') ) {
+				megaMenu.classList.remove('header__megamenu--active');
+				btn.classList.remove('header__subnav-close');
+			} else {
+				megaMenu.classList.add('header__megamenu--active');
+				btn.classList.add('header__subnav-close');
+			}
+		}
+	}
+}
+
+headerNavBtn.onclick = function () {
+	if ( this.classList.contains('header__nav-btn--close') ) {
+		this.classList.remove('header__nav-btn--close');
+		headerNav.classList.remove('header__nav--active');
+	} else {
+		this.classList.add('header__nav-btn--close');
+		headerNav.classList.add('header__nav--active');
 	}
 }
